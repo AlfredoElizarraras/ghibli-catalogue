e58e0888aa54bdc0eefbebf8eedc61ace63376e3
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-14 05:13:11 -0600

    Add hooks to root component to fetch movie data.

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
+import MoviesList from '../../containers/MoviesList';
+import request from '../../utils/requests';
+import addMovies from '../../redux/actions/moviesActions';
 
-const Root = ({ store }) => (
-  <Provider store={store}>
-    Ghibli Catalogue
-  </Provider>
-);
+const Root = ({ store }) => {
+  useEffect(async () => {
+    const response = request();
+    response.then(data => {
+      addMovies(data.data);
+    });
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <MoviesList />
+    </Provider>
+  );
+};
 
 Root.propTypes = {
   store: PropTypes.object.isRequired, /* eslint-disable-line react/forbid-prop-types */
