9b426c5dc46e90e2e6937652e2bd99f4b974af8c
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-14 04:06:43 -0600

    Create Filter component.

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 'ALL',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = filter => {
+    const { handleFilterChange, type } = this.props;
+    handleFilterChange({ [type]: filter });
+    this.setState({ selected: filter });
+  }
+
+  render() {
+    const { selected } = this.state;
+    const { data, type } = this.props;
+    return (
+      <>
+        <label htmlFor={type}>
+          Fiter by
+          {' '}
+          { type }
+          <select
+            id={type}
+            type={type}
+            name={type}
+            onChange={e => this.handleChange(e.target.value)}
+            value={selected}
+          >
+            <option>ALL</option>
+            {data.map(dataInfo => (
+              <option key={dataInfo} value={dataInfo}>
+                {dataInfo.toUpperCase()}
+              </option>
+            ))}
+          </select>
+        </label>
+      </>
+    );
+  }
+}
+
+Filter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.string,
+  ).isRequired,
+};
+
+export default Filter;
