afe11d1da5f271c7e7e1c500eec0e407b1668d9d
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-18 10:51:07 -0600

    Change Movie component to get from the url the movie Id and display the movie info.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './movie.module.css';
+import { Link } from 'react-router-dom';
+import movieImage from '../../utils/data';
 
 const Movie = ({ movie }) => (
-  <div className={styles.movie}>
-    <span>{ movie.title }</span>
-    <span>{ movie.description }</span>
-    <span>{ movie.director }</span>
-    <span>{ movie.producer }</span>
-    <span>{ movie.release_date }</span>
-    <span>{ movie.rt_score }</span>
-    <span>{ movie.url }</span>
-  </div>
+  <>
+    <nav>
+      <Link to="/">
+        <img src="https://cdn.iconscout.com/icon/free/png-512/back-arrow-1767531-1502435.png" alt="back to movies" />
+      </Link>
+
+      <h1>{ movie.title }</h1>
+    </nav>
+
+    <div>
+      <div>
+        <img alt={movie.title} src={movieImage[movie.id]} />
+      </div>
+      <div />
+      <div>
+        <div>
+          <div>
+            <div>Release year</div>
+            { movie.release_date }
+          </div>
+          <div />
+          <div>
+            <div>Score:</div>
+            { movie.rt_score }
+            / 100
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>Director:</div>
+            { movie.director }
+          </div>
+          <div />
+          <div>
+            <div>Producer:</div>
+            { movie.producer }
+          </div>
+        </div>
+      </div>
+      <div>
+        { movie.description }
+      </div>
+    </div>
+  </>
 );
 
 Movie.propTypes = {
--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,1 +1,12 @@
-export { default } from './Movie';
+import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getMovie } from '../../redux/selectors/selectors';
+import Movie from './Movie';
+
+const mapStateToProps = state => {
+  const { movieId } = useParams();
+
+  return { movie: getMovie(state, movieId)[0] };
+};
+
+export default connect(mapStateToProps, null)(Movie);
