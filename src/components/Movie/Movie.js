94e9878b2e7467da85d67e906f0d488a4d0f149a
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-18 11:12:51 -0600

    Style Movie component.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,49 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import movieImage from '../../utils/data';
+import styles from './movie.module.css';
 
 const Movie = ({ movie }) => (
   <>
-    <nav>
+    <nav className={styles.navbar}>
       <Link to="/">
-        <img src="https://cdn.iconscout.com/icon/free/png-512/back-arrow-1767531-1502435.png" alt="back to movies" />
+        <img className={styles.arrow} src="https://cdn.iconscout.com/icon/free/png-512/back-arrow-1767531-1502435.png" alt="back to movies" />
       </Link>
 
       <h1>{ movie.title }</h1>
     </nav>
 
-    <div>
-      <div>
-        <img alt={movie.title} src={movieImage[movie.id]} />
+    <div className={styles.movie}>
+      <div className={styles.imageContainer}>
+        <img className={styles.movieImage} alt={movie.title} src={movieImage[movie.id]} />
       </div>
-      <div />
-      <div>
-        <div>
+      <div className={styles.movieInfo}>
+        <div className={styles.movieInfoHighLights}>
           <div>
-            <div>Release year</div>
-            { movie.release_date }
+            <div>
+              <div>Release year</div>
+              { movie.release_date }
+            </div>
+            <div className={styles.separator} />
+            <div>
+              <div>Score:</div>
+              { movie.rt_score }
+              / 100
+            </div>
           </div>
-          <div />
           <div>
-            <div>Score:</div>
-            { movie.rt_score }
-            / 100
+            <div>
+              <div>Director:</div>
+              { movie.director }
+            </div>
+            <div className={styles.separator} />
+            <div>
+              <div>Producer:</div>
+              { movie.producer }
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <div>Director:</div>
-            { movie.director }
-          </div>
-          <div />
-          <div>
-            <div>Producer:</div>
-            { movie.producer }
-          </div>
-        </div>
-      </div>
-      <div>
-        { movie.description }
+        <div>{ movie.description }</div>
       </div>
     </div>
   </>
