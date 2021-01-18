deaafe750ea89716184f1dcbdff759af9ee3330f
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2021-01-18 10:36:14 -0600

    Change Filter Component to use hooks.

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,52 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './filter.module.css';
 
-class Filter extends React.Component {
-  constructor(props) {
-    super(props);
+const Filter = ({ type, handleFilterChange, data }) => {
+  const [filter, setFilter] = useState('ALL');
 
-    this.state = {
-      filter: 'ALL',
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = filter => {
-    const { handleFilterChange } = this.props;
+  const handleChange = filter => {
     handleFilterChange(filter);
-    this.setState({ filter });
-  }
+    setFilter(filter);
+  };
 
-  render() {
-    const { filter } = this.state;
-    const { data, type } = this.props;
-
-    return (
-      <>
-        <label htmlFor={type} className={styles.filter}>
-          Fiter by
-          {' '}
-          { type }
-          <select
-            id={type}
-            type={type}
-            name={type}
-            onChange={e => this.handleChange(e.target.value)}
-            value={filter}
-          >
-            <option>ALL</option>
-            {data.map(dataInfo => (
-              <option key={`${dataInfo}${type.charAt(0).toUpperCase() + type.slice(1)}`} value={dataInfo}>
-                {dataInfo.toUpperCase()}
-              </option>
-            ))}
-          </select>
-        </label>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <label htmlFor={type} className={styles.filter}>
+        Fiter by
+        {' '}
+        { type }
+        <select
+          id={type}
+          type={type}
+          name={type}
+          onChange={e => handleChange(e.target.value)}
+          value={filter}
+        >
+          <option>ALL</option>
+          {data.map(dataInfo => (
+            <option key={`${dataInfo}${type.charAt(0).toUpperCase() + type.slice(1)}`} value={dataInfo}>
+              {dataInfo.toUpperCase()}
+            </option>
+          ))}
+        </select>
+      </label>
+    </>
+  );
+};
 
 Filter.propTypes = {
   handleFilterChange: PropTypes.func.isRequired,
